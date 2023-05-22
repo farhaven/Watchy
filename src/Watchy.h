6ef952c7de7f9a987c13aea3b5a52d1ce43f3971
Gregor Best <gbe@unobtanium.de>
2023-05-22 21:03:54 +0200

    Don't sort includes
    They're order-dependent

--- src/Watchy.h
+++ src/Watchy.h
@@ -17,71 +17,68 @@
 #include "config.h"
 
 typedef struct weatherData {
-  int8_t temperature;
-  int16_t weatherConditionCode;
-  bool isMetric;
-  String weatherDescription;
-  bool external;
+	int8_t temperature;
+	int16_t weatherConditionCode;
+	bool isMetric;
+	String weatherDescription;
+	bool external;
 } weatherData;
 
 typedef struct watchySettings {
-  // Weather Settings
-  String cityID;
-  String weatherAPIKey;
-  String weatherURL;
-  String weatherUnit;
-  String weatherLang;
-  int8_t weatherUpdateInterval;
-  // NTP Settings
-  String ntpServer;
-  int gmtOffset;
-  //
-  bool vibrateOClock;
+	// Weather Settings
+	String cityID;
+	String weatherAPIKey;
+	String weatherURL;
+	String weatherUnit;
+	String weatherLang;
+	int8_t weatherUpdateInterval;
+	// NTP Settings
+	String ntpServer;
+	int gmtOffset;
+	//
+	bool vibrateOClock;
 } watchySettings;
 
 class Watchy {
-public:
-  static WatchyRTC RTC;
-  static GxEPD2_BW<WatchyDisplay, WatchyDisplay::HEIGHT> display;
-  tmElements_t currentTime;
-  watchySettings settings;
+  public:
+	static WatchyRTC RTC;
+	static GxEPD2_BW<WatchyDisplay, WatchyDisplay::HEIGHT> display;
+	tmElements_t currentTime;
+	watchySettings settings;
 
-public:
-  explicit Watchy(const watchySettings &s) : settings(s) {} // constructor
-  void init(String datetime = "");
-  void deepSleep();
-  static void displayBusyCallback(const void *);
-  float getBatteryVoltage();
-  void vibMotor(uint8_t intervalMs = 100, uint8_t length = 20);
+  public:
+	explicit Watchy(const watchySettings &s) : settings(s) {} // constructor
+	void init(String datetime = "");
+	void deepSleep();
+	static void displayBusyCallback(const void *);
+	float getBatteryVoltage();
+	void vibMotor(uint8_t intervalMs = 100, uint8_t length = 20);
 
-  virtual void handleButtonPress();
-  void showMenu(byte menuIndex, bool partialRefresh);
-  void showFastMenu(byte menuIndex);
-  void showAbout();
-  void showBuzz();
-  void showAccelerometer();
-  void showSyncNTP();
-  bool syncNTP();
-  bool syncNTP(long gmt);
-  bool syncNTP(long gmt, String ntpServer);
-  void setTime();
-  void setupWifi();
-  bool connectWiFi();
-  weatherData getWeatherData();
-  weatherData getWeatherData(String cityID, String units, String lang,
-                             String url, String apiKey, uint8_t updateInterval);
+	virtual void handleButtonPress();
+	void showMenu(byte menuIndex, bool partialRefresh);
+	void showFastMenu(byte menuIndex);
+	void showAbout();
+	void showBuzz();
+	void showAccelerometer();
+	void showSyncNTP();
+	bool syncNTP();
+	bool syncNTP(long gmt);
+	bool syncNTP(long gmt, String ntpServer);
+	void setTime();
+	void setupWifi();
+	bool connectWiFi();
+	weatherData getWeatherData();
+	weatherData getWeatherData(String cityID, String units, String lang, String url, String apiKey, uint8_t updateInterval);
 
-  void showWatchFace(bool partialRefresh);
-  virtual void drawWatchFace(); // override this method for different watch
-                                // faces
+	void showWatchFace(bool partialRefresh);
+	virtual void drawWatchFace(); // override this method for different watch
+	                              // faces
 
-private:
-  void _bmaConfig();
-  static void _configModeCallback(WiFiManager *myWiFiManager);
-  static uint16_t _readRegister(uint8_t address, uint8_t reg, uint8_t *data,
-                                uint16_t len);
-  static uint16_t _writeRegister(uint8_t address, uint8_t reg, uint8_t *data,
-                                 uint16_t len);
+  private:
+	void _bmaConfig();
+	static void _configModeCallback(WiFiManager *myWiFiManager);
+	static uint16_t _readRegister(uint8_t address, uint8_t reg, uint8_t *data, uint16_t len);
+	static uint16_t _writeRegister(uint8_t address, uint8_t reg, uint8_t *data, uint16_t len);
 };
 
 extern RTC_DATA_ATTR int guiState;
